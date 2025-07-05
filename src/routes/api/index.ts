431bcfbfd306a79c1666989a67729143acbd1d73
odinellefsen <odin@ellefsen.fo>
2025-07-05 18:05:48 +0100

    Update database name in docker-compose and enhance API routes for recipe handling

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from "hono";
+import recipe from "./recipe";
 import transformer from "./transformer";
 
 export const api = new Hono();
 
 api.get("/", (c) => {
-  return c.text("Daily Scheduler API");
+    return c.text("Daily Scheduler API");
 });
 
-api.route("/transformer/", transformer);
+api.route("/transformer", transformer);
+
+api.route("/recipe", recipe);
 
 export default api;
--- src/routes/api/recipe/index.ts
+++ src/routes/api/recipe/index.ts
@@ -1,49 +1,20 @@
-import type { FlowcoreLegacyEvent } from "@flowcore/pathways";
 import { Hono } from "hono";
-import { zodEnv } from "../../../../env";
-import { pathwaysRouter } from "../../../utils/flowcore";
+import { foodRecipeEventContract } from "../../../contracts/recipe";
 
 export const recipe = new Hono();
 
 recipe.post("/", async (c) => {
     try {
-        const event = (await c.req.json()) as FlowcoreLegacyEvent;
-        const secret = c.req.header("X-Secret");
+        const body = await c.req.json();
 
-        console.log("Received event", {
-            flowType: event.flowType,
-            eventType: event.eventType,
-            eventId: event.eventId,
+        const parsedBody = foodRecipeEventContract.parse(body);
+
+        console.log("Parsed body", parsedBody);
+
+        return c.json({
+            message: "Recipe created ✅",
         });
-
-        if (secret !== zodEnv.FLOWCORE_WEBHOOK_API_KEY) {
-            return c.json(
-                {
-                    error: "Unauthorized",
-                    message: "The secret key is incorrect",
-                },
-                401
-            );
-        }
-
-        await pathwaysRouter.processEvent(event, secret);
-
-        return c.json(
-            {
-                message: "Event processed ✅",
-            },
-            200
-        );
-    } catch (error) {
-        console.error("Error processing event", { error });
-        return c.json(
-            {
-                error: "Failed to process event",
-                message: (error as Error).message,
-            },
-            500
-        );
-    }
+    } catch (error) {}
 });
 
 export default recipe;
