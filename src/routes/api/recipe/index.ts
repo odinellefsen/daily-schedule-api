698155d6c24e56c6cd6c3e19832a0d0406d70aeb
odinellefsen <odin@ellefsen.fo>
2025-07-06 14:46:51 +0100

    Add recipe existence check in API route; return error if recipe already exists

--- src/routes/api/recipe/index.ts
+++ src/routes/api/recipe/index.ts
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { foodRecipeEventContract } from "../../../contracts/recipe";
 import { db } from "../../../db";
+import { recipes } from "../../../db/schema";
+import { FlowcorePathways } from "../../../utils/flowcore";
 
 export const recipe = new Hono();
 
@@ -9,7 +12,22 @@ recipe.post("/", async (c) => {
         const body = await c.req.json();
         const parsedBody = foodRecipeEventContract.parse(body);
 
-        console.log("Parsed body", parsedBody);
+        const existingRecipe = await db.query.recipes.findFirst({
+            where: eq(recipes.id, parsedBody.id),
+        });
+
+        if (existingRecipe) {
+            return c.json(
+                {
+                    message: "Recipe already exists ❌",
+                },
+                400
+            );
+        }
+
+        FlowcorePathways.write("recipe.v0/recipe.created.v0", {
+            data: parsedBody,
+        });
 
         return c.json({
             message: "Recipe created ✅",
