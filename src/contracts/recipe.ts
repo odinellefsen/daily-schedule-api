e0bf5e2b6e4bf9e69ed67734def48c8003acda5a
odinellefsen <odin@ellefsen.fo>
2025-07-06 14:33:57 +0100

    Refactor foodRecipeEventContract schema to use baseFoodRecipeEventSchema for improved structure and clarity; update FlowcorePathways registration to utilize the new schema.

--- src/contracts/recipe.ts
+++ src/contracts/recipe.ts
@@ -51,134 +51,131 @@ export enum MealTimingEnum {
     SUPPER = "Supper",
 }
 
-export const foodRecipeEventContract = z
-    .object({
-        id: z.string().uuid("The ID must be a valid UUID"),
-        whenIsMealEaten: z.nativeEnum(MealTimingEnum),
-        nameOfTheFoodRecipe: z
-            .string()
-            .min(1, "The name of the food recipe is required")
-            .max(
-                75,
-                "The name of the food recipe must be less than 75 characters"
-            ),
-        generalDescriptionOfTheFoodRecipe: z
-            .string()
-            .min(
-                1,
-                "If generalDescriptionOfTheFoodRecipe is NOT undefined, you must have at least one character"
-            )
-            .max(
-                250,
-                "The general description of the food recipe must be less than 250 characters"
-            )
-            .optional(),
-        ingredientsOfTheFoodRecipe: z
-            .array(
-                z.object({
-                    id: z
-                        .string()
-                        .uuid("The ID of the ingredient must be a valid UUID"),
-                    nameOfTheIngredient: z
-                        .string()
-                        .min(1, "The ingredient name is required")
-                        .max(
-                            50,
-                            "The ingredient name must be less than 50 characters"
-                        ),
-                    quantityOfTheIngredient: z
-                        .number()
-                        .positive("Quantity must be greater than 0"),
-                    unitOfMeasurementOfTheIngredientQuantity: z.nativeEnum(
-                        UnitOfMeasurementEnum
+const baseFoodRecipeEventSchema = z.object({
+    id: z.string().uuid("The ID must be a valid UUID"),
+    whenIsMealEaten: z.nativeEnum(MealTimingEnum),
+    nameOfTheFoodRecipe: z
+        .string()
+        .min(1, "The name of the food recipe is required")
+        .max(75, "The name of the food recipe must be less than 75 characters"),
+    generalDescriptionOfTheFoodRecipe: z
+        .string()
+        .min(
+            1,
+            "If generalDescriptionOfTheFoodRecipe is NOT undefined, you must have at least one character"
+        )
+        .max(
+            250,
+            "The general description of the food recipe must be less than 250 characters"
+        )
+        .optional(),
+    ingredientsOfTheFoodRecipe: z
+        .array(
+            z.object({
+                id: z
+                    .string()
+                    .uuid("The ID of the ingredient must be a valid UUID"),
+                nameOfTheIngredient: z
+                    .string()
+                    .min(1, "The ingredient name is required")
+                    .max(
+                        50,
+                        "The ingredient name must be less than 50 characters"
                     ),
-                })
-            )
-            .min(
-                1,
-                "If ingredientsOfTheFoodRecipe is NOT undefined, you must have at least one ingredient"
-            )
-            .optional(),
-        stepForStepInstructionsToMakeTheFoodRecipe: z
-            .array(
-                z.object({
-                    stepNumber: z
-                        .number()
-                        .positive("Step number must be greater than 0")
-                        .int("Step number must be an integer"),
-                    instruction: z
-                        .string()
-                        .min(1, "The instruction is required")
-                        .max(
-                            250,
-                            "The instruction must be less than 150 characters"
-                        ),
-                    ingredientsUsedInThisStep: z
-                        .array(
-                            z.object({
-                                id: z
-                                    .string()
-                                    .uuid(
-                                        "The ID of the ingredient used in a step must be a valid UUID"
-                                    ),
-                                nameOfTheIngredientUsedInThisStep: z
-                                    .string()
-                                    .min(1, "The ingredient name is required")
-                                    .max(
-                                        50,
-                                        "The ingredient name must be less than 50 characters"
-                                    ),
-                                quantityOfTheIngredientUsedInThisStep: z
-                                    .number()
-                                    .positive(
-                                        "Quantity used in this step must be greater than 0"
-                                    ),
-                                unitOfMeasurementOfTheIngredientQuantityUsedInThisStep:
-                                    z.nativeEnum(UnitOfMeasurementEnum),
-                            })
-                        )
-                        .min(
-                            1,
-                            "If ingredientsUsedInThisStep is NOT undefined, you must have at least one ingredient"
-                        )
-                        .optional(),
-                })
-            )
-            .min(
-                1,
-                "If stepForStepInstructionsToMakeTheFoodRecipe is NOT undefined, you must have at least one step"
-            )
-            .optional(),
-    })
-    .refine(
-        (data) => {
-            if (!data.stepForStepInstructionsToMakeTheFoodRecipe) {
-                // If there are no steps by step instructions, we don't need to check if ingredients are used in steps
-                return true;
-            }
+                quantityOfTheIngredient: z
+                    .number()
+                    .positive("Quantity must be greater than 0"),
+                unitOfMeasurementOfTheIngredientQuantity: z.nativeEnum(
+                    UnitOfMeasurementEnum
+                ),
+            })
+        )
+        .min(
+            1,
+            "If ingredientsOfTheFoodRecipe is NOT undefined, you must have at least one ingredient"
+        )
+        .optional(),
+    stepForStepInstructionsToMakeTheFoodRecipe: z
+        .array(
+            z.object({
+                stepNumber: z
+                    .number()
+                    .positive("Step number must be greater than 0")
+                    .int("Step number must be an integer"),
+                instruction: z
+                    .string()
+                    .min(1, "The instruction is required")
+                    .max(
+                        250,
+                        "The instruction must be less than 150 characters"
+                    ),
+                ingredientsUsedInThisStep: z
+                    .array(
+                        z.object({
+                            id: z
+                                .string()
+                                .uuid(
+                                    "The ID of the ingredient used in a step must be a valid UUID"
+                                ),
+                            nameOfTheIngredientUsedInThisStep: z
+                                .string()
+                                .min(1, "The ingredient name is required")
+                                .max(
+                                    50,
+                                    "The ingredient name must be less than 50 characters"
+                                ),
+                            quantityOfTheIngredientUsedInThisStep: z
+                                .number()
+                                .positive(
+                                    "Quantity used in this step must be greater than 0"
+                                ),
+                            unitOfMeasurementOfTheIngredientQuantityUsedInThisStep:
+                                z.nativeEnum(UnitOfMeasurementEnum),
+                        })
+                    )
+                    .min(
+                        1,
+                        "If ingredientsUsedInThisStep is NOT undefined, you must have at least one ingredient"
+                    )
+                    .optional(),
+            })
+        )
+        .min(
+            1,
+            "If stepForStepInstructionsToMakeTheFoodRecipe is NOT undefined, you must have at least one step"
+        )
+        .optional(),
+});
 
-            // Get all ingredient names
-            const ingredientNames =
-                data.ingredientsOfTheFoodRecipe?.map(
-                    (ing) => ing.nameOfTheIngredient
-                ) ?? [];
-
-            // Check if all ingredient's names used in steps exist in ingredients array
-            return (
-                data.stepForStepInstructionsToMakeTheFoodRecipe?.every(
-                    (step) =>
-                        step.ingredientsUsedInThisStep?.every(
-                            (usedIngredient) =>
-                                ingredientNames.includes(
-                                    usedIngredient.nameOfTheIngredientUsedInThisStep
-                                )
-                        ) ?? true
-                ) ?? true
-            );
-        },
-        {
-            message:
-                "The field values of nameOfTheIngredientUsedInThisStep must also be defined in the ingredientsOfTheFoodRecipe array",
-            path: ["stepForStepInstructionsToMakeTheFoodRecipe"],
+export { baseFoodRecipeEventSchema };
+export const foodRecipeEventContract = baseFoodRecipeEventSchema.refine(
+    (data) => {
+        if (!data.stepForStepInstructionsToMakeTheFoodRecipe) {
+            // If there are no steps by step instructions, we don't need to check if ingredients are used in steps
+            return true;
         }
-    );
+
+        // Get all ingredient names
+        const ingredientNames =
+            data.ingredientsOfTheFoodRecipe?.map(
+                (ing) => ing.nameOfTheIngredient
+            ) ?? [];
+
+        // Check if all ingredient's names used in steps exist in ingredients array
+        return (
+            data.stepForStepInstructionsToMakeTheFoodRecipe?.every(
+                (step) =>
+                    step.ingredientsUsedInThisStep?.every((usedIngredient) =>
+                        ingredientNames.includes(
+                            usedIngredient.nameOfTheIngredientUsedInThisStep
+                        )
+                    ) ?? true
+            ) ?? true
+        );
+    },
+    {
+        message:
+            "The field values of nameOfTheIngredientUsedInThisStep must also be defined in the ingredientsOfTheFoodRecipe array",
+        path: ["stepForStepInstructionsToMakeTheFoodRecipe"],
+    }
+);
--- src/utils/flowcore.ts
+++ src/utils/flowcore.ts
@@ -1,36 +1,36 @@
 import {
-  createPostgresPathwayState,
-  PathwayRouter,
-  PathwaysBuilder,
+    createPostgresPathwayState,
+    PathwayRouter,
+    PathwaysBuilder,
 } from "@flowcore/pathways";
 import { zodEnv } from "../../env";
-import { recipeSchema } from "../contracts/recipe";
+import { baseFoodRecipeEventSchema } from "../contracts/recipe";
 import { handlerRecipeCreated } from "../services/HandleTest";
 
 export const postgresUrl = zodEnv.POSTGRES_CONNECTION_STRING;
 const webhookApiKey = zodEnv.FLOWCORE_WEBHOOK_API_KEY;
 
 export const FlowcorePathways = new PathwaysBuilder({
-  baseUrl: zodEnv.FLOWCORE_WEBHOOK_BASE_URL,
-  tenant: zodEnv.FLOWCORE_TENANT,
-  dataCore: zodEnv.FLOWCORE_DATA_CORE_NAME,
-  apiKey: webhookApiKey,
+    baseUrl: zodEnv.FLOWCORE_WEBHOOK_BASE_URL,
+    tenant: zodEnv.FLOWCORE_TENANT,
+    dataCore: zodEnv.FLOWCORE_DATA_CORE_NAME,
+    apiKey: webhookApiKey,
 })
-  .withPathwayState(
-    createPostgresPathwayState({
-      connectionString: postgresUrl,
-    }),
-  )
-  .register({
-    flowType: "recipe.v0",
-    eventType: "recipe.created.v0",
-    schema: recipeSchema,
-    writable: true,
-  });
+    .withPathwayState(
+        createPostgresPathwayState({
+            connectionString: postgresUrl,
+        })
+    )
+    .register({
+        flowType: "recipe.v0",
+        eventType: "recipe.created.v0",
+        schema: baseFoodRecipeEventSchema,
+        writable: true,
+    });
 
 FlowcorePathways.handle("recipe.v0/recipe.created.v0", handlerRecipeCreated);
 
 export const pathwaysRouter = new PathwayRouter(
-  FlowcorePathways,
-  webhookApiKey,
+    FlowcorePathways,
+    webhookApiKey
 );
