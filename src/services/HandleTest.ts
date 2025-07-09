f7d5dafb1c4b57d0427d518086a66d0d4028d2a2
odinellefsen <odin@ellefsen.fo>
2025-07-09 21:34:04 +0100

    Add recipe event handlers for creation, update, and deletion; enhance logging for better event tracking

--- src/services/HandleTest.ts
+++ src/services/HandleTest.ts
@@ -1,4 +1,5 @@
 import type { FlowcoreEvent } from "@flowcore/pathways";
+import { eq } from "drizzle-orm";
 import type z from "zod";
 import type { foodRecipeEventContract } from "../contracts/recipe";
 import { db } from "../db";
@@ -9,11 +10,38 @@ export async function handlerRecipeCreated(
         payload: z.infer<typeof foodRecipeEventContract>;
     }
 ) {
-    console.log("received an event ✅", event);
+    console.log("received an recipe created event ✅", event);
 
     await db.insert(recipes).values({
         id: event.payload.id,
         name: event.payload.nameOfTheFoodRecipe,
         description: event.payload.generalDescriptionOfTheFoodRecipe || null,
     });
+}
+
+export async function handlerRecipeUpdated(
+    event: Omit<FlowcoreEvent, "payload"> & {
+        payload: z.infer<typeof foodRecipeEventContract>;
+    }
+) {
+    console.log("received an recipe updated event ✅", event);
+
+    await db
+        .update(recipes)
+        .set({
+            name: event.payload.nameOfTheFoodRecipe,
+            description:
+                event.payload.generalDescriptionOfTheFoodRecipe || null,
+        })
+        .where(eq(recipes.id, event.payload.id));
+}
+
+export async function handlerRecipeDeleted(
+    event: Omit<FlowcoreEvent, "payload"> & {
+        payload: z.infer<typeof foodRecipeEventContract>;
+    }
+) {
+    console.log("received an recipe deleted event ✅", event);
+
+    await db.delete(recipes).where(eq(recipes.id, event.payload.id));
 }
