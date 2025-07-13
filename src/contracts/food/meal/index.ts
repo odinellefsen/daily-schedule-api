26012ce7f803b6676845d0984b138239b85c2753
odinellefsen <odin@ellefsen.fo>
2025-07-13 18:42:06 +0100

    Remove meal cancelled contract and update index file to improve clarity and organization.

--- src/contracts/food/meal/index.ts
+++ src/contracts/food/meal/index.ts
@@ -1,3 +1,2 @@
 export * from "./meal.contract";
-export * from "./meal-cancelled.contract";
 export * from "./meal-instructions.contract";
--- src/contracts/food/meal/meal-cancelled.contract.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-// This schema is used to create and update a cancelled meal
-export const mealCancelledSchema = z.object({
-    mealId: z.string().uuid(),
-    reasonWhyMealWasCancelled: z.string().min(1).max(250).optional(),
-});
-
-export type MealCancelledType = z.infer<typeof mealCancelledSchema>;
