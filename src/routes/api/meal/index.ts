0547b592ae9501284f6f853e5e3578df48bcc647
odinellefsen <odin@ellefsen.fo>
2025-10-22 10:48:53 +0100

    refactor: Remove unused meal handlers and routes to streamline meal management API

--- src/routes/api/meal/index.ts
+++ src/routes/api/meal/index.ts
@@ -1,14 +1,7 @@
 import { Hono } from "hono";
 import { requireAuth } from "../../../middleware/auth";
 import { registerCreateMeal } from "./meal.create";
-import { registerDeleteMeal } from "./meal.delete";
 import { registerListMeals } from "./meal.list";
-import { registerCreateMealIngredients } from "./meal-ingredients.create";
-import { registerDeleteMealIngredients } from "./meal-ingredients.delete";
-import { registerPatchMealIngredients } from "./meal-ingredients.patch";
-import { registerCreateMealInstructions } from "./meal-instructions.create";
-import { registerDeleteMealInstructions } from "./meal-instructions.delete";
-import { registerPatchMealInstructions } from "./meal-instructions.patch";
 
 export const meal = new Hono();
 
@@ -16,13 +9,6 @@ meal.use("/*", requireAuth());
 
 // Register all meal routes
 registerCreateMeal(meal);
-registerDeleteMeal(meal);
 registerListMeals(meal);
-registerCreateMealIngredients(meal);
-registerDeleteMealIngredients(meal);
-registerPatchMealIngredients(meal);
-registerCreateMealInstructions(meal);
-registerDeleteMealInstructions(meal);
-registerPatchMealInstructions(meal);
 
 export default meal;
