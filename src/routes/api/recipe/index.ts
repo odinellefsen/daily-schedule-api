49bd74dd144cd267c4e5f811069f65bd93bf1d3c
odinellefsen <odin@ellefsen.fo>
2025-11-16 11:46:56 +0000

    fix: fixed import path

--- src/routes/api/recipe/index.ts
+++ src/routes/api/recipe/index.ts
@@ -3,13 +3,8 @@ import { requireAuth } from "../../../middleware/auth";
 import { registerCreateRecipe } from "./recipe.create";
 import { registerDeleteRecipe } from "./recipe.delete";
 import { registerListRecipes } from "./recipe.list";
-import { registerPatchRecipe } from "./recipe.patch";
 import { registerCreateRecipeIngredients } from "./recipe-ingredients.create";
-import { registerDeleteRecipeIngredients } from "./recipe-ingredients.delete";
-import { registerPatchRecipeIngredients } from "./recipe-ingredients.patch";
 import { registerCreateRecipeInstructions } from "./recipe-instructions.create";
-import { registerDeleteRecipeInstructions } from "./recipe-instructions.delete";
-import { registerPatchRecipeInstructions } from "./recipe-instructions.patch";
 
 export const recipe = new Hono();
 
@@ -19,12 +14,7 @@ recipe.use("/*", requireAuth());
 registerCreateRecipe(recipe);
 registerDeleteRecipe(recipe);
 registerListRecipes(recipe);
-registerPatchRecipe(recipe);
 registerCreateRecipeIngredients(recipe);
-registerDeleteRecipeIngredients(recipe);
-registerPatchRecipeIngredients(recipe);
 registerCreateRecipeInstructions(recipe);
-registerDeleteRecipeInstructions(recipe);
-registerPatchRecipeInstructions(recipe);
 
 export default recipe;
