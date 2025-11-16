56244eadff5d8300406daec353f6519409e95ce6
odinellefsen <odin@ellefsen.fo>
2025-11-16 10:54:36 +0000

    refactor: Remove patch routes for food items and food item units to streamline API structure

--- src/routes/api/food-item/index.ts
+++ src/routes/api/food-item/index.ts
@@ -3,11 +3,9 @@ import { requireAuth } from "../../../middleware/auth";
 import { registerCreateFoodItem } from "./food-item.create";
 import { registerDeleteFoodItem } from "./food-item.delete";
 import { registerListFoodItems } from "./food-item.list";
-import { registerPatchFoodItem } from "./food-item.patch";
 import { registerCreateFoodItemUnits } from "./food-item-units.create";
 import { registerDeleteFoodItemUnits } from "./food-item-units.delete";
 import { registerListFoodItemUnits } from "./food-item-units.list";
-import { registerPatchFoodItemUnits } from "./food-item-units.patch";
 
 export const foodItem = new Hono();
 
@@ -15,11 +13,9 @@ foodItem.use("/*", requireAuth());
 
 // Register all food item routes
 registerCreateFoodItem(foodItem);
-registerPatchFoodItem(foodItem);
 registerDeleteFoodItem(foodItem);
 registerListFoodItems(foodItem);
 registerCreateFoodItemUnits(foodItem);
-registerPatchFoodItemUnits(foodItem);
 registerDeleteFoodItemUnits(foodItem);
 registerListFoodItemUnits(foodItem);
 
