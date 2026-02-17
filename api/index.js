e0d7d6badd8e4ede9330232d8cbf72a6c73f6193
odinellefsen <odin@ellefsen.fo>
2026-02-17 20:00:06 +0000

    chore: Update dependencies and refactor API handler by replacing JavaScript file with TypeScript version

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const { handle } = require("hono/vercel");
-
-// `dist/index.js` is produced during Vercel build (see `vercel-build` script).
-const { app } = require("../dist/index.js");
-
-module.exports = handle(app);
-
