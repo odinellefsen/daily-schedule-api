81b284ef5510d9bd12ac990ee7e8960afe88f175
odinellefsen <odin@ellefsen.fo>
2026-02-22 21:46:16 +0000

    refactor: Remove unused API routes to streamline the API structure

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,9 +1,4 @@
 import { Hono } from "hono";
-import foodItem from "./food-item";
-import habit from "./habit";
-import meal from "./meal";
-import recipe from "./recipe";
-import todo from "./todo";
 import transformer from "./transformer";
 
 export const api = new Hono();
@@ -13,11 +8,5 @@ api.get("/", (c) => {
 });
 
 api.route("/transformer/", transformer);
-
-api.route("/food-item", foodItem);
-api.route("/habit", habit);
-api.route("/meal", meal);
-api.route("/recipe", recipe);
-api.route("/todo", todo);
 
 export default api;
